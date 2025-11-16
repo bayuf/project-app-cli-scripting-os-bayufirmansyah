fcd4c39b2a55afe73b5c034524e1cbff39adc19b
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-11-16 10:48:05 +0700

    add: menambahkan input pengguna dan logika permainan

--- app.sh
+++ app.sh
@@ -11,7 +11,7 @@ esac
 
 clear
 
-while [ true ]; do
+while ( true ); do
     # Menu Game
     echo " _________________________________"
     echo "|== Selamat Datang di Permainan ==|"
@@ -24,19 +24,60 @@ while [ true ]; do
     echo "|_________________________________|"
 
     pilihMenu=0
+    echo ""
     read -p "Pilih : " pilihMenu
 
         # Validasi Input Pilihan Menu
-        if [ "$pilihMenu" -lt 1 ] || [ "$pilihMenu" -gt 2 ]; then
+        if [[ "$pilihMenu" -lt 1 || "$pilihMenu" -gt 2 ]]; then
             clear
             echo "Input Salah Silahkan pilih antara angka 1 atau 2"
             continue
         fi
 
         case $pilihMenu in
-            1) echo "Mulai";;
-            2) echo "Keluar";;
+            1) clear 
+                break;;
+            2) clear
+                echo "Terima Kasih Sudah Bermain..."
+                exit;;
         esac
 done
 
 
+
+    echo " _________________________________________"
+    echo "|== Musuhmu sudah menentukan pilihannya ==|"
+    echo "|==           Ini Giliranmu..!!!        ==|"
+    echo "|====     Gunting, Batu, Kertas       ====|"
+    echo "|                                         |"
+    echo "|              1. Gunting                 |"
+    echo "|              2. Batu                    |"
+    echo "|              3. Kertas                  |"
+    echo "|_________________________________________|"
+    
+    playerPil=0
+    echo ""
+    read -p "Tentukan pihanmu sekarang : " playerPil
+    playerChoice=""
+
+    case $playerPil in
+        1) playerChoice="Gunting";;
+        2) playerChoice="Batu";;
+        3) playerChoice="Kertas";;
+    esac
+
+    echo "Hasilnya adalah"
+    echo "Kamu Memilih     : $playerChoice"
+    echo "Computer Memilih : $comChoice"
+    echo ""
+
+    # Jika pilihan player dan Computer sama maka Hasilnya Draw dan keduanya tidak dapat poin
+    if [[ "$playerChoice" == "$comChoice" ]]; then
+        echo "Woow Hasilnya Seri"
+    # Jika Player Menang akan Mendapat poin 10
+    elif [[ ("$playerChoice" == "Gunting" && "$comChoice" == "Kertas") || ("$playerChoice" == "Batu" && "$comChoice" == "Gunting") || ("$playerChoice" == "Kertas" && "$comChoice" == "Batu") ]]; then
+        echo "Selamat Kamu Menang"
+    # Jika Player kalah maka komputer akan mendapat Poin 10
+    else 
+        echo "Sayang sekali Kamu Kalah"
+    fi
